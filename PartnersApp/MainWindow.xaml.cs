5a1244e045b776ae8e047dc214520daafc1d02ff
Olimp10 <Olimp10@D205PC15>
2024-10-12 15:49:23 +0300

    Добавьте файлы проекта.

---
+++ PartnersApp/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PartnersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Entities entity = new Entities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            var Login = LoginBox.Text;
+            var Pass = PassBox.Text;
+
+
+            if (entity.User.First(x => x.Login == Login).Login == Login)
+            {
+                
+                if (entity.User.First(x => x.Password == Pass).Password == Pass)
+                {
+                    Partners partnerWindow = new Partners();
+                    partnerWindow.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин");
+            }
+
+
+        }
+    }
+}
---
+++ PartnersApp/Partners.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PartnersApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Partners.xaml
+    /// </summary>
+    public partial class Partners : Window
+    {
+        Entities entity = new Entities();
+        public Partners()
+        {
+            InitializeComponent();
+
+            PartnersView.ItemsSource = entity.Partner.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddPartner addPartner = new AddPartner();
+            addPartner.Show();
+            this.Close();
+        }
+    }
+}
